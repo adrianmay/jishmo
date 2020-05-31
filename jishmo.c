9f70c4fd513868cb92933e7109a5c78ca18e3537
Adrian May <adrian.alexander.may@gmail.com>
2020-05-31 09:25:03 +0100

    Use avformat and prove link works

--- jishmo.c
+++ jishmo.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
 #include <libavutil/motion_vector.h>
+#include <libavformat/avformat.h>
 
-AVMotionVector soTheIncludeWorksButWhatAboutTheLink;
+static AVFormatContext *fmt_ctx = NULL;
 
-int main() {
-  printf("Hello, world!\n");
-  return 0;
+int extract_motion_vectors(char *src_filename){
+  int ret = 0;
+  if (avformat_open_input(&fmt_ctx, src_filename, NULL, NULL) < 0) { //So the link works too
+    fprintf(stderr, "Could not open source file %s\n", src_filename);
+    exit(1);
+  }
+}
+
+int main(int argc, char **argv) { 
+  return extract_motion_vectors(argv[1]); 
 }
 
 
